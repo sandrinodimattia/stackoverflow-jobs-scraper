8a96b4ec38b7a66072d9226e348fcb702bc69c8f
Sandrino Di Mattia <contact@sandrino.be>
2017-12-16 02:12:19 +0100

    Initial implementation of the cli

---
+++ README.md
@@ -0,0 +1,11 @@
+# StackOverflow Jobs Scraper
+
+A small CLI tool that allows you to download all jobs for a given location which contain salary information.
+
+Usage:
+
+```bash
+npm run jobs -- -l Amsterdam -f amsterdam-jobs.csv
+```
+
+![](./media/screenshot.png)
---
+++ index.js
@@ -0,0 +1,47 @@
+const debug = require('debug')('crawler');
+const puppeteer = require('puppeteer');
+const commander = require('commander');
+
+const csv = require('./lib/export-csv');
+const StackOverflowJobs = require('./lib/jobs');
+
+commander
+  .option('-f, --file', 'File (eg: jobs.csv)')
+  .option('-l, --location', 'The location where to find a job (eg: Amsterdam)')
+  .parse(process.argv);
+
+debug.enabled = true;
+
+async function run(location, filePath) {
+  try {
+    debug('Starting puppeteer');
+
+    const browser = await puppeteer.launch({
+      headless: true,
+    });
+    const page = await browser.newPage();
+
+    debug('Getting jobs');
+
+    const so = new StackOverflowJobs(page);
+    const links = await so.getLinks(location);
+
+    const jobs = await so.getJobs(links);
+    csv(filePath, jobs);
+
+    debug('Exported jobs to %s', filePath);
+    console.log('\n');
+
+    await page.close();
+    await browser.close();
+  } catch (e) {
+    debug('error', e);
+  }
+}
+
+if (commander.args.length !== 2) {
+  commander.help();
+  console.log('\n');
+} else {
+  run(...commander.args);
+}
